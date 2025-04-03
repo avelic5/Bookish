e402ef63fd14200552cf7e951a2219890dc83838
Aldin Velić <avelic5@etf.unsa.ba>
2025-04-03 14:14:30 +0100

    Dodana tema i ispravljene neke greske

--- app/src/main/java/com/example/bookish/MainActivity.kt
+++ app/src/main/java/com/example/bookish/MainActivity.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -83,7 +84,7 @@ fun BookishAppLayout() {
             TextField(
                 value = searchQuery,
                 onValueChange = { searchQuery = it },
-                label = { Text("Search by title") },
+                label = { Text(stringResource(R.string.search_by_title)) },
                 modifier = Modifier.weight(1f)
             )
 
@@ -114,7 +115,7 @@ fun EditNameField(
     modifier: Modifier = Modifier
 ) {
     TextField(
-        label = { Text("Book title")},
+        label = { Text(stringResource(R.string.book_title))},
         value = value,
         onValueChange = onValueChange,
         modifier = modifier
